3839afd6909dc8fe7302fa67c0eedb9ba7f166db
Emily <31485900+emurph1@users.noreply.github.com>
2019-09-18 21:20:25 -0500

    Delete dailyCodeTwitter.py

--- dailyCodeTwitter.py
+++
@@ -1,9 +0,0 @@
-query= "de"
-words = ["dog", "deer", "deal"]
-newWords = []
-def findW(que):
-    for item in words:
-        if item[:len(que)] == que:
-            newWords.append(item)
-    return newWords
-print(findW(query))
